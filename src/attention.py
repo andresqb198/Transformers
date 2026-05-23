1a5d1ff6bfb561ed78c04569cf730874e6aff074
andresqb198 <andres.quintero1228@gmail.com>
2026-05-22 21:55:11 -0500

    [DELETE] Normalization

--- src/attention.py
+++ src/attention.py
@@ -8,8 +8,7 @@ class TransformedLayer:
         self._X = X
 
     def get_attentions_scores(self) -> Array:
-        dimension_key = self._X.shape[-1]
-        return (self._X @ self._X.T) / dimension_key
+        return (self._X @ self._X.T)
     
     def get_attention_weights(self):
         scores = self.get_attentions_scores()
